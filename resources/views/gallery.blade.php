a217b4049ceacb0c799e97230aca317ad85cda2c
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-06 20:54:55 +0100

    Added style sheets

--- resources/views/gallery.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-
-@section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1>My Galleries</h1>
-        </div>
-    </div>
-@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Image Gallery
                 </div>
 
                 <div class="links">
