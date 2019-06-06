3bf2e879b4008f63d9e8a50c6f588fc0a1491b05
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-06 20:53:37 +0100

    Added styles

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,10 +2,18 @@
 <html>
     <head>
         <title>My Gallery App</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/cerulean/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     </head>
     <body>
         <div class="container">
             @yield('content')
         </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/dropzone.js" type="text/javascript"></script>
     </body>
 </html>
