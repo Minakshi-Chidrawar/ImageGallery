e7cc081b27cd3a0eec8c63a3909a4ad8fecf4e1a
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-07 05:20:13 +0100

    Amended to show data from table

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -1,11 +1,49 @@
 @extends('master')
 
 @section('content')
-    <div class="container">
+
     <div class="row">
         <div class="col-md-12">
             <h1>My Galleries</h1>
         </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8">
+            @if ($galleries->count() > 0)
+                <table class="table table-striped table-responsive">
+                    <thead>
+                        <tr>
+                            <th>Name of the Gallery</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($galleries as $gallery)
+                        <tr>
+                            <td>{{ $gallery->name }}</td>
+                            <td>View</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+        </div>
+
+
+    <div class="col-md-4">
+        <form method="post" action="{{ url('gallery/save') }}" class="form">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <input type="text" name="gallery_name"
+                    id='gallery_name' placeholder='Name of the Gallery'
+                class="form-control" />
+            </div>
+
+            <button class="btn btn-primary">Save</button>
+        </form>
     </div>
     </div>
 @endsection
