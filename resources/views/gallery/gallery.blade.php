2172b0c021ce96b7d4e20cc79a79d9edef343bcb
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-08 23:44:00 +0100

    Added validation for gallery name

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -11,15 +11,15 @@
     <div class="row">
         <div class="col-md-8">
             @if ($galleries->count() > 0)
-                <table class="table table-striped table-responsive">
-                    <thead>
+                <table class="table table-striped table-responsive table-width">
+                    <thead class="table-width">
                         <tr>
                             <th>Name of the Gallery</th>
                             <th></th>
                         </tr>
                     </thead>
 
-                    <tbody>
+                    <tbody class="table-width">
                         @foreach ($galleries as $gallery)
                         <tr>
                             <td>{{ $gallery->name }}</td>
@@ -30,20 +30,28 @@
                 </table>
             @endif
         </div>
-
-
-    <div class="col-md-4">
-        <form method="post" action="{{ url('gallery/save') }}" class="form">
-            {{ csrf_field() }}
-
-            <div class="form-group">
-                <input type="text" name="gallery_name"
-                    id='gallery_name' placeholder='Name of the Gallery'
-                class="form-control" />
-            </div>
-
-            <button class="btn btn-primary">Save</button>
-        </form>
+    
+        <div class="col-md-4">
+        @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+        @endif
+            <form method="post" action="{{ url('gallery/save') }}" class="form">
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                    <input type="text" name="gallery_name"
+                        id='gallery_name' placeholder='Name of the Gallery'
+                    class="form-control" />
+                </div>
+
+                <button class="btn btn-primary">Save</button>
+            </form>
+        </div>
     </div>
 @endsection
