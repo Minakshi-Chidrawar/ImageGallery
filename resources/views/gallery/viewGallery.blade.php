c419976ec8bb0fb8f151cbfe6e8e47c8d491e895
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-09 22:42:42 +0100

    Amended to add images

--- resources/views/gallery/viewGallery.blade.php
+++ resources/views/gallery/viewGallery.blade.php
@@ -9,6 +9,15 @@
 
     <div class="row">
         <div class="col-md-12">
+            <form action="{{ url('image/do-upload')}}"
+                class="dropzone" id="addImages">
+                {{ csrf_field() }}
+                <input type="hidden" name="gallery_id" value="{{ $gallery->id }}" >
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
             <a href="{{ url('gallery/list') }}" class="btn btn-primary">Back</a>
         </div>
     </div>
